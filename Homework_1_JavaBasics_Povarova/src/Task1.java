e9abfc16c39aac2bed52e034e29d9bd1ccdb701c
annisnotok <povarovaanna@gmail.com>
2021-08-24 15:00:45 +0300

    Task 1 and Task 2 done!

--- Homework_1_JavaBasics_Povarova/src/Task1.java
+++ Homework_1_JavaBasics_Povarova/src/Task1.java
@@ -1,50 +1,68 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Task1 {
 
-    private static void printArray(ArrayList<Integer> array)
+    private static void printArray(int[] array)
     {
         for(int arr:array)
         {
-            System.out.println(arr + " ");
+            System.out.println(arr);
         }
         System.out.println();
     }
 
-    private static ArrayList<Integer> createIntArray(int length){
+    private static int[] createIntArray(int length){
         Random rnd = new Random();
-        ArrayList<Integer> arr = new ArrayList<>(length);
+        int[] arr = new int[length];
         for(int i = 0; i < length; i++){
-            arr.add(rnd.nextInt(100));
+            arr[i] = rnd.nextInt(100);
         }
         return arr;
     }
 
     //Сортировка целочисленного массива методом пузырька
-    private static void bubbleSort(ArrayList<Integer> arr){
-        for(int i=0; i < arr.size(); i++) {
-            for (int j = arr.size() - 1; j > i; j--) {
-                if (arr.get(j - 1) > arr.get(j)) {
-                    int temp = arr.get(j - 1);
-                    arr.set(j - 1, arr.get(j));
-                    arr.set(j, temp);
+    private static void bubbleSort(int[] arr){
+        for(int i=0; i < arr.length; i++) {
+            for (int j = arr.length - 1; j > i; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] =  arr[j];
+                    arr[j] =  temp;
                 }
             }
         }
     }
 
+    private static int BinarySearch(int[] arr, int first, int last, int el){
+        int index = -1, middle;
+        while (first <= last){
+            middle = (first + last) / 2;
+            if (arr[middle] > el)
+                last = middle - 1;
+            else if (arr[middle] < el)
+                first = middle + 1;
+            else if (arr[middle] == el) {
+                index = middle;
+                break;
+            }
+        }
+        return index;
+    }
+
     public static void main(String[] args){
         //Сортировка пузырьком
-        ArrayList<Integer> a1 = createIntArray(7);
+        int[] a1 = createIntArray(7);
         printArray(a1);
-        //bubbleSort(a1);
+        bubbleSort(a1);
         printArray(a1);
-        //Сортировка бинарный поиск
-        ArrayList<Integer> a2 = createIntArray(5);
+        //Бинарный поиск
+        int[] a2 = {67, 31, 62, 78, 94, 23, 48, 9, 0, 54, 6, 92};
+        bubbleSort(a2);
         printArray(a2);
-        //BinarySearchSort(a2);
-        printArray(a2);
+        System.out.println("Элемент со значением 6 находится под индексом " + BinarySearch(a2, 0, a2.length-1,6));
     }
 
 }
